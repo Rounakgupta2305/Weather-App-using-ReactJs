31c3a7ae46b4e471588467374f7d370632e4d818
Rounak Gupta <Rounak.1si21ec080@gmail.com>
2024-04-28 03:20:05 +0530

    Update info.js

--- src/Services/info.js
+++ src/Services/info.js
@@ -1,12 +1,39 @@
+import {DateTime} from "luxon"
 const key="12649df7ba78a474a31fb7144a849823"
-const baseurl = `https://api.openweathermap.org/data/2.5`
+const baseurl = `https://api.openweathermap.org/data/`
 
 const getWeatherData = (infoType, searchParams)=> {
-  const url = new URL (baseurl + "/" +infoType);
-  url.search = new URLSearchParams ({... searchParams, appid:key}
-  )
-  return fetch(url).then((res)=> res.json());
+
+  if(infoType=="weather"){
+    const url = new URL (baseurl + "2.5" + "/" + infoType);
+    url.search = new URLSearchParams ({... searchParams, appid:key})
+    return fetch(url).then((res)=> res.json());
+  }
+
+  if(infoType=="onecall"){
+    const url = new URL (baseurl + "3.0" + "/" + infoType);
+    url.search = new URLSearchParams ({... searchParams, appid:key})
+    return fetch(url).then((res)=> res.json());
+  }
+
 };
+
+const formatCurrentWeather = (data) => {
+  const {
+    coord: {lat, lon},
+    main: {temp, feels_like, temp_min, temp_max, humidity},
+    name,
+    dt,
+    sys: {country, sunrise, sunset}, 
+    weather, 
+    wind: {speed}
+  } = data;
+
+  const {main: details, icon}=weather[0]
+
+  return {lat, lon, temp, feels_like, temp_min, temp_max, humidity,
+  name, dt, country, sunrise, sunset, details, icon, speed}
+}
 
 
 const formatForecastWeather =(data) => {
@@ -30,27 +57,12 @@ const formatForecastWeather =(data) => {
 };
 
 
-const formatCurrentWeather = (data) => {
-  const {
-    coord: {lat, lon},
-    main: {temp, feels_like, temp_min, temp_max, humidity},
-    name,
-    dt,
-    sys: {country, sunrise, sunset}, 
-    weather, 
-    wind: {speed}
-  } = data;
-
-  const {main: details, icon}=weather[0]
-
-  return {lat, lon, temp, feels_like, temp_min, temp_max, humidity,
-  name, dt, country, sunrise, sunset, details, icon, speed}
-}
-
-
-
 const getFormattedWeatherData = async (searchParams)=> {
-  const formattedCurrentWeather= await getWeatherData("weather", searchParams).then(formatCurrentWeather)
+  const formattedCurrentWeather= await getWeatherData(
+    "weather", 
+    searchParams
+  ).then(formatCurrentWeather); 
+  
   const {lat, lon} = formattedCurrentWeather;
 
   const formattedForecastWeather = await getWeatherData("onecall", {
@@ -58,16 +70,21 @@ const getFormattedWeatherData = async (searchParams)=> {
     lon, 
     exclude: "current,minutely,alerts", 
     units: searchParams.units
-  }).then (formatForecastWeather);
-
+    }).then (formatForecastWeather);
+ 
   return {...formattedCurrentWeather, ...formattedForecastWeather};
-}
-const formatToLocalTime = (secs, zone, format= "cccc, dd LLL yyy' | Local time: 'hh:mm a")=> DataTime.fromSeconds(secs).setZone(zone).toFormat(format);
+};
 
-const iconUrlFromCode= (code)=>{
-  `https://openweathermap.org/img/wn/${code}@2x.png`
-}
+const formatToLocalTime = (
+  secs, 
+  zone, 
+  format= "cccc, dd LLL yyyy' | Local time: 'hh:mm a"
+) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
 
 export default getFormattedWeatherData;
 
-export {formatToLocalTime, iconUrlFromCode}
+const iconUrlFromCode = (code) =>
+  `http://openweathermap.org/img/wn/${code}@2x.png`;
+
+
+export { formatToLocalTime, iconUrlFromCode };
