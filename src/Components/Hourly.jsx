12746a1214c1181fab3d2ba3747ccb45f2a6f024
Rounak Gupta <Rounak.1si21ec080@gmail.com>
2024-04-28 03:20:57 +0530

    Update Hourly.jsx

--- src/Components/Hourly.jsx
+++ src/Components/Hourly.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { iconUrlFromCode } from '../Services/info'
 
 function Hourly({title, items}) {
+  
   return (
     <>
     <div className='text-white flex items-center justify-start mt-4'>
@@ -12,14 +13,14 @@ function Hourly({title, items}) {
 
     <div className="flex flex-row items-center justify-between text-white mt-5">
 
-      {items.map(item => (
-        <div className="flex flex-col items-center justify-center">
-          <p className='font-light'>{item.tile}</p>
-          <img src={iconUrlFromCode(item.icon)} alt='' className='w-10'/>
-          <p className='font-light'>{`${item.temp.toFixed()}°`}</p>
-        </div>
+      {items.map((item, index) => (
+        <div key={index} className="flex flex-col items-center justify-center">
+        <p className='font-light'>{item.title}</p>
+        <img src={iconUrlFromCode(item.icon)} alt='' className='w-10'/>
+        <p className='font-light'>{`${item.temp.toFixed()}°`}</p>
+      </div>
       ))}
-
+      
     </div>
     </>
   )
