bb9702e7a39e3a291246c08291c180c587604bd4
Rounak Gupta <Rounak.1si21ec080@gmail.com>
2024-04-28 03:19:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import Inputs from './Components/Inputs'
 import Time from './Components/Time'
 import Hourly from './Components/Hourly'
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
-import getFormattedWeatherData from './Services/info'
 import { useEffect, useState } from 'react'
+import getFormattedWeatherData from './Services/info'
+
 
 function App() {
   const [query, setQuery]= useState({q:'berlin'})
@@ -13,31 +14,29 @@ function App() {
 
   useEffect(() => {
     const fetchWeather = async()=>{
-      getFormattedWeatherData({...query, units}).then(
+      await getFormattedWeatherData({...query, units}).then(
         (data)=> {
           setWeather(data)
+          console.log(data)
         });
     }
-    fetchWeather()
-    
-     
-  }, [query, units])
-  
 
-  
-  
+    fetchWeather()
+  }, [query, units])
+
   return (
     <>
       <div className='flex-row justify-center items-center mx-auto max-w-screen-md mt-4  h-fit backdrop-filter backdrop-blur-md md:backdrop-blur-lg shadow-md shadow-gray-400 p-5 rounded-lg'> 
         <h1 className='text-center text-white text-5xl font-mono	font-family:"Liberation Mono" pt-0 '>Weather App</h1>
-        <Inputs setQuery={setQuery} units={units} setUnits={setUnits}/>
-        {weather &&(
+        <Inputs setQuery={setQuery} units={units} setUnits={setUnits}/> 
+
+        {weather && (
           <div>
             <Time weather={weather}/>
-            <Hourly title="Hourly Forecast" items= {weather.hourly}/>
-            <Hourly title="Daily Forecast" items= {weather.daily}/>
+            <Hourly title="Hourly Forecast" items={weather.hourly}/>
+            <Hourly title="Daily Forecast" items={weather.daily}/>
           </div>
-        )}  
+        )}
       </div>
     </>
   )
